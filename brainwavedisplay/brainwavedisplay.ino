56469a37199382ac5bb81d2764327b158ef28257
Nathan D <99107654+ndhann@users.noreply.github.com>
2022-02-26 17:35:03 -0500

    add rudimentary display capabilities

--- brainwavedisplay/brainwavedisplay.ino
+++ brainwavedisplay/brainwavedisplay.ino
@@ -1,4 +1,15 @@
-#define SAMPLEINTERVAL 7.8125
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+#include <SPI.h>
+#include <Wire.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET 4
+#define SCREEN_ADDRESS 0x3C
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define SAMPLEINTERVAL 7.8125 // for sample rate of 128Hz
 double currentTime;
 double sampleTime;
 
@@ -25,8 +36,46 @@ void readAllInputs(double delta, double theta, double alpha, double beta) {
   beta = analogRead(pinBeta);
 }
 
+void clearDisplayInputValues() {
+  display.setCursor(0, 0);
+  display.println("               ");
+  display.println("               ");
+  display.println("               ");
+  display.println("               ");
+}
+
+void printInputValues(double delta, double theta, double alpha, double beta) {
+  clearDsiplayInputValues();
+  display.setCursor(0, 0);
+  
+  display.print("Delta: ");
+  display.println(delta);
+
+  display.print("Theta: ");
+  display.println(theta);
+
+  display.print("Alpha: ");
+  display.println(alpha);
+
+  display.print("Beta: ");
+  display.println(beta);
+
+  display.display();
+}
+
 void setup() {
-  // put your setup code here, to run once:
+  // serial console
+  Serial.begin(9600);
+
+  // set up display
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;); // Don't proceed, loop forever
+  }
+  display.clearDisplay()
+  display.setTextColor(WHITE);
+
+  // timer setup
   currentTime = millis();
   sampleTime = currentTime + SAMPLEINTERVAL;
 }
@@ -38,6 +87,8 @@ void loop() {
     currentTime = millis();
     sampleTime = currentTime + SAMPLEINTERVAL;
   }
+
+  printInputValues(inputDelta, inputTheta, inputAlpha, inputBeta);
 
   currentTime = millis();
 }
