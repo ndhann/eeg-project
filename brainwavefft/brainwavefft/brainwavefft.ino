bf51f3cfa49701ffed05568e17aa4dd9974ac94a
Nathan D <99107654+ndhann@users.noreply.github.com>
2022-03-27 17:00:28 -0400

    Adds code to display calculated powers

--- brainwavefft/brainwavefft/brainwavefft.ino
+++ brainwavefft/brainwavefft/brainwavefft.ino
@@ -21,20 +21,29 @@ unsigned long curTime;
 double inputReal[SAMPLES];
 double inputImag[SAMPLES];
 
+double deltapower = 0.0;
+double thetapower = 0.0;
+double alphapower = 0.0;
+double betapower = 0.0;
+
 const int inputPin = A1;
 
 void setup() {
   Serial.begin(115200);
 
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;); // Don't proceed, loop forever
+  }
+  display.clearDisplay();
+  display.setTextColor(WHITE, BLACK);
+
+
   // obtain the sampling period in microseconds
   sampling_period_micro = round(1000000 * (1.0 / SAMPLES));
 }
 
-void printPowers(double input[]) {
-  double deltapower = 0.0;
-  double thetapower = 0.0;
-  double alphapower = 0.0;
-  double betapower = 0.0;
+void calculatePowers(double input[]) {
   for (int i = 0; i <= 3; i++) {
     deltapower += input[i] * input[i];
   }
@@ -47,6 +56,9 @@ void printPowers(double input[]) {
   for (int i = 13; i <= 30; i++) {
     betapower += input[i] * input[i];
   }
+}
+void printPowers(double input[]) {
+  calculatePowers(input);
 
   Serial.print("Delta wave power is: ");
   Serial.println(deltapower);
@@ -58,6 +70,89 @@ void printPowers(double input[]) {
   Serial.println(betapower);
 }
 
+void drawBar(int x, int y, double power, double maxpower) {
+  if (((power / maxpower) > (0.8))) {
+    display.drawChar(x + (4 * 7), y, 0xDA, WHITE, BLACK, 1);
+  }
+
+  if (((power / maxpower) > (0.6))) {
+    display.drawChar(x + (3 * 7), y, 0xDA, WHITE, BLACK, 1);
+  }
+
+  if (((power / maxpower) > (0.4))) {
+    display.drawChar(x + (2 * 7), y, 0xDA, WHITE, BLACK, 1);
+  }
+
+  if (((power / maxpower) > (0.2))) {
+    display.drawChar(x + (1 * 7), y, 0xDA, WHITE, BLACK, 1);
+  }
+
+  if (((power / maxpower) > (0.1))) {
+    display.drawChar(x + (0 * 7), y, 0xDA, WHITE, BLACK, 1);
+  }
+}
+
+double findMaxPower(double deltapower, double thetapower, double alphapower, double betapower) {
+  double maxpower = 0.0;
+  if (maxpower < deltapower) {
+    maxpower = deltapower;
+  }
+  if (maxpower < thetapower) {
+    maxpower = thetapower;
+  }
+  if (maxpower < alphapower) {
+    maxpower = alphapower;
+  }
+  if (maxpower < betapower) {
+    maxpower = betapower;
+  }
+}
+
+void drawBarGraph(double deltapower, double thetapower, double alphapower, double betapower) {
+  double maxpower = findMaxPower(deltapower, thetapower, alphapower, betapower);
+  drawBar(80, 8, deltapower, maxpower);
+  drawBar(80, 16, thetapower, maxpower);
+  drawBar(80, 24, alphapower, maxpower);
+  drawBar(80, 32, betapower, maxpower);
+}
+
+void clearDisplayedPowers() {
+  display.setCursor(0, 0);
+  display.setTextWrap(false);
+  // clear the displayed values by simply overwriting them with blank text
+  display.println("                        ");
+  display.println("                        ");
+  display.println("                        ");
+  display.println("                        ");
+  display.println("                        ");
+  display.setTextWrap(true);
+}
+
+void displayPowers(double input[]) {
+  calculatePowers(input);
+
+  clearDisplayedPowers();
+  display.setCursor(0, 0);
+
+  display.println("Power");
+
+  display.print("Delta : ");
+  display.println(deltapower);
+
+  display.print("Theta: ");
+  display.println(thetapower);
+
+  display.print("Alpha: ");
+  display.println(alphapower);
+
+  display.print("Beta: ");
+  display.println(betapower);
+
+  drawBarGraph(deltapower, thetapower, alphapower, betapower);
+
+  display.display();
+}
+
 void loop() {
   for (int i = 0; i < SAMPLES; i++) {
     curTime = micros();
@@ -67,12 +162,12 @@ void loop() {
       // do nothing until it's time to sample again
     }
   }
-  
+
   // calculate the FFT
   FFT.Compute(inputReal, inputImag, SAMPLES, FFT_FORWARD);
   double peak = FFT.MajorPeak(inputReal, SAMPLES, SAMPLES);
-  Serial.print("FFT peak is: ");
-  Serial.println(peak);
+  //Serial.print("FFT peak is: ");
+  //Serial.println(peak);
 
-  printPowers(inputReal);
+  displayPowers(inputReal);
 }
