0ff11be2c7dd9973ed5249e823ebb1b6bdbc9fd8
Nathan D <99107654+ndhann@users.noreply.github.com>
2022-03-29 15:13:06 -0400

    Add explanatory comments

--- brainwavefft/brainwavefft/brainwavefft.ino
+++ brainwavefft/brainwavefft/brainwavefft.ino
@@ -9,6 +9,7 @@
 #define I2C_ADDRESS 0x3C
 SSD1306AsciiWire oled;
 
+// calculated from the ADC ranges (0-5V and 0-1023)
 #define VOLTSPERBIT 0.0049
 #define SAMPLES 128
 
@@ -25,6 +26,7 @@ double thetapower = 0.0;
 double alphapower = 0.0;
 double betapower = 0.0;
 
+// The pin to receive the signal to process
 const int inputPin = A1;
 
 void setup() {
@@ -34,6 +36,7 @@ void setup() {
 
   oled.begin(&Adafruit128x64, I2C_ADDRESS, RST_PIN);
   oled.clear();
+  // Set font to something small so everything can be displayed on screen.
   oled.setFont(System5x7);
 
   // obtain the sampling period in microseconds
@@ -46,26 +49,33 @@ void calculatePowers() {
   alphapower = 0.0;
   betapower = 0.0;
 
+  // calculate the RMS for each frequency range
+  // Delta: 1-3Hz
   for (int i = 1; i <= 3; i++) {
     deltapower += sq(inputReal[i]) + sq(inputImag[i]);
   }
   deltapower = sqrt(deltapower / 3.0);
 
+  // Theta: 4-8Hz
   for (int i = 4; i <= 8; i++) {
     thetapower += sq(inputReal[i]) + sq(inputImag[i]);
   }
   thetapower = sqrt(thetapower / 5.0);
 
+  // Alpha: 9-12Hz
   for (int i = 9; i <= 12; i++) {
     alphapower += sq(inputReal[i]) + sq(inputImag[i]);
   }
   alphapower = sqrt(alphapower / 4.0);
 
+  // Beta: 13-30Hz
   for (int i = 13; i <= 30; i++) {
     betapower += sq(inputReal[i]) + sq(inputImag[i]);
   }
   betapower = sqrt(betapower / 18.0);
 }
+
+// This function prints to the serial terminal. Used for debugging.
 void printPowers() {
   calculatePowers();
 
@@ -79,6 +89,7 @@ void printPowers() {
   Serial.println(betapower);
 }
 
+// Draws a bar using ASCII characters based on the two quantities given
 void drawBar(double power, double maxpower) {
   if (((power / maxpower) > (0.8))) {
     oled.print("|");
@@ -101,6 +112,7 @@ void drawBar(double power, double maxpower) {
   }
 }
 
+// Simple algorithm to find the largest power
 double findMaxPower(double deltapower, double thetapower, double alphapower, double betapower) {
   double maxpower = 0.0;
   if (maxpower < deltapower) {
@@ -118,15 +130,18 @@ double findMaxPower(double deltapower, double thetapower, double alphapower, dou
   return maxpower;
 }
 
+// displays values and the bar graph on the oled display
 void displayPowers() {
   calculatePowers();
   double maxpower = findMaxPower(deltapower, thetapower, alphapower, betapower);
   Serial.println(maxpower);
 
+  // clear the screen every time calculations are refreshed
   oled.clear();
 
   oled.println("Power");
 
+  // write out values and draw bars for each of the frequency ranges
   oled.print("Delta: ");
   oled.print(deltapower);
   oled.print(" ");
@@ -153,10 +168,11 @@ void displayPowers() {
 }
 
 void loop() {
+  // loop and obtain 128 samples over one second
   for (int i = 0; i < SAMPLES; i++) {
     curTime = micros();
-    inputReal[i] = analogRead(inputPin) * VOLTSPERBIT;
-    inputImag[i] = 0;
+    inputReal[i] = analogRead(inputPin) * VOLTSPERBIT; // read the ADC value and convert to volts
+    inputImag[i] = 0; // no imaginary input
     while (micros() < (curTime + sampling_period_micro)) {
       // do nothing until it's time to sample again
     }
